745e2e3b1ec12bf761d4d913cf7f9d5a49d7c6b2
unknown <khcharem.omar@gmail.com>
2025-09-21 10:04:17 +0100

    Description des changements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -47,7 +47,10 @@ const Navbar = () => {
           >
             <button
               type="button"
-              className="text-white bg-[#524dd3] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 sm:hidden md:block"
+              className="text-white bg-gradient-to-r from-blue-500 via-cyan-400 to-teal-400
+               shadow shadow-cyan-400/50
+                active:scale-95
+               transition-transform duration-200 focus:ring-4 focus:ring-blue-300 font-medium rounded-3xl text-sm px-4 py-2 sm:hidden md:block"
             >
               Become a Member
             </button>
